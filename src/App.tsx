d9ab0e8b58af4bdc17704db710950449d5e35f7f
sabirKhan97 <khansabir5400@gmail.com>
2025-12-27 20:53:07 +0530

    add whole code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import TypingSpeedTest from "/home/sabir/Codespaces/typing-time/src/pages/MainPage/TypingAll.tsx"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <TypingSpeedTest/>
+    </>
+  )
+}
+
+export default App
